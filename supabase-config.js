de5f6a11dd94c0dc507980d75b527d77420ec848
rezz678 <frexardaxf@gmail.com>
2026-02-19 12:28:04 +0700

    Update Supabase URL and anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // js/supabase-config.js
-const SUPABASE_URL = 'https://vdcsnfjxjfhcgoeivxrc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkY3NuZmp4amZoY2dvZWl2eHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNTQ3MzMsImV4cCI6MjA4NjkzMDczM30.XWGIHJMIoyy73xWmmPYqDcfj3FFDg3_UezAcbAbevZU';
+const SUPABASE_URL = 'https://lpuvhisdsdcetctyuevv.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_nxFnlwP4XHIwG-gPtJvkuQ_AOXLGTaa.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkY3NuZmp4amZoY2dvZWl2eHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNTQ3MzMsImV4cCI6MjA4NjkzMDczM30.XWGIHJMIoyy73xWmmPYqDcfj3FFDg3_UezAcbAbevZU';
 
 // Inisialisasi Supabase
 window.supabase = window.supabase || {};
